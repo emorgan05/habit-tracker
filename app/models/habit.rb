261262b832bbb67abf45c21c11d37951720f920c
emorgan05 <emorgan05@gmail.com>
2018-03-02 13:11:02 -0500

    Adding relationships to models

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,2 +1,5 @@
 class Habit < ApplicationRecord
+  belongs_to :users
+  belongs_to :categories
+  has_many :stars
 end
--- app/models/star.rb
+++ app/models/star.rb
@@ -1,2 +1,4 @@
 class Star < ApplicationRecord
+  belongs_to :habits
+  delegate :user, :to => :habit, :allow_nil => true
 end
