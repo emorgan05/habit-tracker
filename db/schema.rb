93dc0fed3728bd6222869151fd689cf1f90ab66c
emorgan05 <emorgan05@gmail.com>
2018-03-02 13:56:40 -0500

    Migrated new tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180302175420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "habits", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "number_of_days"
+    t.bigint "user_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_habits_on_category_id"
+    t.index ["user_id"], name: "index_habits_on_user_id"
+  end
+
+  create_table "stars", force: :cascade do |t|
+    t.datetime "date"
+    t.boolean "completed"
+    t.bigint "habit_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["habit_id"], name: "index_stars_on_habit_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
