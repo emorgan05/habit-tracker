61b3552b77296c94b8708593707faa73b91a99ab
emorgan05 <emorgan05@gmail.com>
2018-03-05 14:52:57 -0500

    Moving createStore function to configureStore file and adding thunk

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import rootReducer from './reducers/rootReducer';
 import { Provider } from 'react-redux';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
 ReactDOM.render(
   <Provider store={store}>
     <App store={store}/>
-  </Provider>, 
+  </Provider>,
   document.getElementById('root')
 );
 
