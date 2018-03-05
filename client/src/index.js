3feaf7c0aa3e573dd266376086150e9c1807f798
emorgan05 <emorgan05@gmail.com>
2018-03-05 15:01:26 -0500

    Changing the index.js to properly use configureStore

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
