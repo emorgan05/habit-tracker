bc848dec948c80670f9d4f35ddb2b5a6def18cb0
emorgan05 <emorgan05@gmail.com>
2018-03-05 15:58:58 -0500

    Changing Navigation based on whether user is signed in or not

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,20 +6,40 @@ import { Navbar,
   NavLink } from 'reactstrap';
 
 class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // loggedIn: this.props.loggedIn
+    };
+  }
+
   render () {
-    return (
-      <Navbar color="faded" light expand="md">
-          <NavbarBrand href="/">HabitTracker</NavbarBrand>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="#">Sign Up</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#">Log In</NavLink>
-              </NavItem>
-            </Nav>
-        </Navbar>
-    )
+    // if(loggedIn) {
+    //   return (
+    //     <Navbar color="faded" light expand="md">
+    //         <NavbarBrand href="/">HabitTracker</NavbarBrand>
+    //           <Nav className="ml-auto" navbar>
+    //             <NavItem>
+    //               <NavLink href="#">Log Out</NavLink>
+    //             </NavItem>
+    //           </Nav>
+    //       </Navbar>
+    //   )
+    // } else {
+      return (
+        <Navbar color="faded" light expand="md">
+            <NavbarBrand href="/">HabitTracker</NavbarBrand>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="#">Sign Up</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">Log In</NavLink>
+                </NavItem>
+              </Nav>
+          </Navbar>
+      )
+    // }
   }
 }
 
