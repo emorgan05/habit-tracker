6af48f323d2cacb2555e3520bb4828bcadfdd55c
emorgan05 <emorgan05@gmail.com>
2018-03-05 16:20:06 -0500

    Adding HabitsList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation';
+import HabitsList from './components/HabitsList';
+import { Container, Row, Col } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Container>
+          <Row>
+            <Col xs="6">
+              <HabitsList />
+            </Col>
+            <Col xs="6">
+              <h1>Hello World</h1>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
