7b915a8862f62f0b7f9d9037bb1f2327daa68b84
emorgan05 <emorgan05@gmail.com>
2018-03-06 12:40:00 -0500

    working on fetchHabits function

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,8 +1,13 @@
+import fetch from 'isomorphic-fetch';
+
 export function fetchHabits() {
   return(dispatch) => {
-    dispatch({type: 'START_HABITS_REQUEST'});
+    dispatch({type: 'LOADING_HABITS'});
     return fetch('http://localhost:3001/habits')
-      .then(response => response.json())
-      .then(habits => dispatch({ type: 'ADD_HABITS', habits }));
+      .then(response => {
+        return response.json()
+      }).then(habits => {
+        return dispatch({ type: 'FETCH_HABITS', habits })
+      });
   };
 }
