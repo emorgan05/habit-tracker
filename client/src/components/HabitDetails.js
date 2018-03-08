0b0570519f081f90658bf13dbe37ebc88c44587d
emorgan05 <emorgan05@gmail.com>
2018-03-08 17:20:49 -0500

    Still playing with styling

--- client/src/components/HabitDetails.js
+++ client/src/components/HabitDetails.js
@@ -3,8 +3,8 @@ import React from 'react';
 const HabitDetails = ({selectedHabit}) => {
   return (
     <div className="habit-details">
-      <h5>Name: {selectedHabit.name}</h5>
-      <h5>Description: {selectedHabit.description}</h5>
+      <p>Name: {selectedHabit.name}</p>
+      <p>Description: {selectedHabit.description}</p>
     </div>
   )
 }
