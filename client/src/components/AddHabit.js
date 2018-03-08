da3b00843919dea8ff51f6280917edd26f1d9182
emorgan05 <emorgan05@gmail.com>
2018-03-08 16:12:19 -0500

    Styling the add habit form

--- client/src/components/AddHabit.js
+++ client/src/components/AddHabit.js
@@ -1,34 +1,39 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const AddHabit = () => {
   return (
     <div className="add-habit">
-      <h3>Add a Habit</h3>
-      <form>
-        <label>Name: </label>
-        <input type="text" name="name" />
-
-        <label>Description: </label>
-        <input type="text" name="description" />
-
-        <label>Number of days per week (1 - 7): </label>
-        <input type="text" name="number_of_days" />
-
-        <label>Category: </label>
-        <select>
-          <option value="Emotional">Emotional</option>
-          <option value="Intellectual">Intellectual</option>
-          <option value="Fitness">Fitness</option>
-          <option value="Social">Social</option>
-          <option value="Environmental">Environmental</option>
-          <option value="Financial">Financial</option>
-          <option value="Spiritual">Spiritual</option>
-          <option value="Health">Health</option>
-          <option value="Relationships/Family">Relationships/Family</option>
-        </select>
-
-        <input type="submit" />
-      </form>
+      <h5>Add a Habit</h5>
+      <Form>
+        <FormGroup>
+          <Label>Name: </Label>
+          <Input type="text" name="name" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Description: </Label>
+          <Input type="text" name="description" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Number of days per week (1 - 7): </Label>
+          <Input type="text" name="number_of_days" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Category: </Label>
+           <Input type="select" name="select" id="exampleSelect">
+            <option value="Emotional">Emotional</option>
+            <option value="Intellectual">Intellectual</option>
+            <option value="Fitness">Fitness</option>
+            <option value="Social">Social</option>
+            <option value="Environmental">Environmental</option>
+            <option value="Financial">Financial</option>
+            <option value="Spiritual">Spiritual</option>
+            <option value="Health">Health</option>
+            <option value="Relationships/Family">Relationships/Family</option>
+          </Input>
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
     </div>
   )
 }
