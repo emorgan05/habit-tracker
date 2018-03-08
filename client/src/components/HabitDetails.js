db7203b7827d508e103b7f864d352c57bce3117c
emorgan05 <emorgan05@gmail.com>
2018-03-08 16:40:00 -0500

    css styling

--- client/src/components/HabitDetails.js
+++ client/src/components/HabitDetails.js
@@ -3,8 +3,8 @@ import React from 'react';
 const HabitDetails = ({selectedHabit}) => {
   return (
     <div className="habit-details">
-      <p>{selectedHabit.name}</p>
-      <p>{selectedHabit.description}</p>
+      <h5>Name: {selectedHabit.name}</h5>
+      <h5>Description: {selectedHabit.description}</h5>
     </div>
   )
 }
