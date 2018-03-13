52cd9005364e8ff2bec1822ba8c86dc8384a1cb0
emorgan05 <emorgan05@gmail.com>
2018-03-13 12:26:43 -0400

    Adding logic to StarListItem to show checks or x's based on whether the habit was completed

--- client/src/components/StarListItem.js
+++ client/src/components/StarListItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import FaCircleO from 'react-icons/lib/fa/circle-o';
+import FaTimesCircleO from 'react-icons/lib/fa/times-circle-o';
+import FaCheckCircleO from 'react-icons/lib/fa/check-circle-o';
 
 const StarListItem = ({star}) => {
-  return (
-    <li>
-      <FaCircleO />
-    </li>
-  )
+  let starDate = star.date.getDate();
+  let now = new Date();
+  now.setHours(0, 0, 0, 0);
+  
+  if (starDate < now) {
+    if (star.completed) {
+      return (<li><FaCheckCircleO /></li>)
+    } else {
+      return (<li><FaTimesCircleO /></li>)
+    }
+  } else {
+    return (<li><FaCircleO /></li>)
+  }
 }
 
 export default StarListItem;
