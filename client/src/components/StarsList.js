39ced088bb84a5af26bb34540f458217e5b3dea3
emorgan05 <emorgan05@gmail.com>
2018-03-13 11:34:54 -0400

    Adding plan for StarsList and StarListItem -- for real this time

---
+++ client/src/components/StarsList.js
@@ -0,0 +1,12 @@
+// With the habit component -- render a list of 7 stars. Their value or their key is the date. Needs to be proper so it
+// can say ok, today is Tuesday 3/13, so the list of 7 will start on Sunday 3/11.
+// If it is a future date -- use a circle
+// If it is a past date, check if it has been completed. If yes -- check mark
+//                                                       If no -- X (times) mark
+// If it is today, check if it has been completed. If yes -- check mark
+//                                                 If no -- circle
+// On click of the circle, push that completed === true to the habit.stars array
+// When the date changes, push completed === false to the habit.stars array
+
+// stars list
+// star list item
