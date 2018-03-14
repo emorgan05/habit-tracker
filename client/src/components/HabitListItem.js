e48ee6e6111862e0f0a299cfd804587baf06c585
emorgan05 <emorgan05@gmail.com>
2018-03-14 17:23:24 -0400

    Shifted habit to display only one day/one icon and toggle to a check mark on click

--- client/src/components/HabitListItem.js
+++ client/src/components/HabitListItem.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import StarsList from './StarsList';
+import React, { Component } from 'react';
+import FaCircleO from 'react-icons/lib/fa/circle-o';
+import FaCheckCircleO from 'react-icons/lib/fa/check-circle-o';
+// import StarsList from './StarsList';
 
-const HabitListItem = ({habit, onHabitSelect}) => {
-  return (
-    <li onClick={() => onHabitSelect(habit)}>
-      {habit.name}
-      <StarsList habit={habit} />
-    </li>
-  )
+class HabitListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completed: false
+    }
+  }
+
+  render() {
+    return (
+      <li>
+        <p onClick={() => this.props.onHabitSelect(this.props.habit)}>
+          {this.props.habit.name}
+        </p>
+        <p onClick={() => this.state.completed === true ? null : this.setState({ completed: true })}>
+          {this.state.completed ? <FaCheckCircleO /> : <FaCircleO />}
+        </p>
+      </li>
+    )
+  }
 }
 
 export default HabitListItem
