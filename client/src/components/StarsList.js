59e1b57b730f95269100a76bccb1f786ccb581ed
emorgan05 <emorgan05@gmail.com>
2018-03-15 14:15:23 -0400

    Cleaning up StarsList file

--- client/src/components/StarsList.js
+++ client/src/components/StarsList.js
@@ -1,23 +1,3 @@
-// With the habit component -- render a list of 7 stars. Their value or their key is the date. Needs to be proper so it
-// can say ok, today is Tuesday 3/13, so the list of 7 will start on Sunday 3/11.
-// If it is a future date -- use a circle
-// If it is a past date, check if it has been completed. If yes -- check mark
-//                                                       If no -- X (times) mark
-// If it is today, check if it has been completed. If yes -- check mark
-//                                                 If no -- circle
-// On click of the circle, push that completed === true to the habit.stars array
-// When the date changes, push completed === false to the habit.stars array
-
-// stars list
-// star list item
-
-// from today
-// find monday
-// check this.state.stars to see if there are 7 stars after monday
-// if there are, map over them and create the starListItems
-// if not, add stars
-// then map over them
-
 import React, { Component } from 'react';
 import StarListItem from './StarListItem';
 
@@ -26,9 +6,6 @@ class StarsList extends Component {
     super(props);
 
     this.state = {
-      today: '',
-      monday: '',
-      mondayStar: '',
       stars: [{}, {}, {}, {}, {}, {}, {}],
     };
   }
