a909d43c3a9c70b4388fbca407c8f843c2da2cae
emorgan05 <emorgan05@gmail.com>
2018-03-15 14:34:55 -0400

    Adding onClick function to the current date star

--- client/src/components/StarListItem.js
+++ client/src/components/StarListItem.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FaCircleO from 'react-icons/lib/fa/circle-o';
 import FaTimesCircleO from 'react-icons/lib/fa/times-circle-o';
 import FaCheckCircleO from 'react-icons/lib/fa/check-circle-o';
 
-const StarListItem = ({star}) => {
-  if (star.date) {
-    let starDate = star.date.getDate();
-    let now = new Date();
-    now.setHours(0, 0, 0, 0);
+class StarListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completed: false,
+    };
+  }
 
-    if (starDate < now.getDate()) {
-      if (star.completed) {
-        return (<li><FaCheckCircleO /></li>)
-      } else {
-        return (<li><FaTimesCircleO /></li>)
+  render() {
+    if (this.props.star.date) {
+      let starDate = this.props.star.date.getDate();
+      let now = new Date();
+      now.setHours(0, 0, 0, 0);
+
+      if (starDate < now.getDate()) {
+        if (this.props.star.completed) {
+          return (<li><FaCheckCircleO /></li>)
+        } else {
+          return (<li><FaTimesCircleO /></li>)
+        }
+      } else if (starDate === now.getDate()) {
+        return (
+          <li onClick={() => this.state.completed === true ? null : this.setState({ completed: true })}>
+            {this.state.completed ? <FaCheckCircleO /> : <FaCircleO />}>
+          </li>
+        )
       }
+    } else {
+      return (<li><FaCircleO /></li>)
     }
-  } else {
-    return (<li><FaCircleO /></li>)
   }
 }
 
