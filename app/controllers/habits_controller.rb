3a2ce262dfb530f8fe06af66009b664ab53bd2f1
emorgan05 <emorgan05@gmail.com>
2018-03-15 16:51:39 -0400

    Adding actions and API calls to login the user

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,4 +1,6 @@
 class HabitsController < ApplicationController
+  # before_action :authenticate_user
+  
   def index
     @habits = Habit.all
     render json: @habits
---
+++ client/src/actions/sessionActions.js
@@ -0,0 +1,18 @@
+import SessionApi from '../api/sessionApi';
+
+export function loginSuccess() {
+  return { type: 'LOG_IN_SUCCESS' }
+}
+
+export function loginUser(credentials) {
+  return function(dispatch) {
+    return sessionApi.login(credentials)
+      .then(response => {
+        sessionStorage.setItem('jwt', response.jwt);
+        dispatch(loginSuccess());
+      })
+      .catch(error => {
+        throw(error);
+      });
+  };
+}
