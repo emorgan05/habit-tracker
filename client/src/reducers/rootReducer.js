78e34b2bf45e5af27d81167006acb4f8c19c60d5
emorgan05 <emorgan05@gmail.com>
2018-03-15 17:22:20 -0400

    Creating sessionReducer and adding it to rootReducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import sessionReducer from './sessionReducer';
 import { habitsReducer } from './habitsReducer';
 // import all the other reducers
 
 const rootReducer = combineReducers({
+  session: sessionReducer,
   habits: habitsReducer
   // name reducers
 });
