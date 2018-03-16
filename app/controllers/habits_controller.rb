be1ab174b400efeb93727c70163faacce0efe788
emorgan05 <emorgan05@gmail.com>
2018-03-16 14:55:49 -0400

    Adding create function to habits controller

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -6,5 +6,16 @@ class HabitsController < ApplicationController
     render json: @habits
   end
 
+  def create
+    @habit = Habit.new(habit_params)
+    if @habit.save
+      render json: @habit
+    end
+  end
+
+  private
   
+  def habit_params
+    params.require(:habit).permit(:name, :description, :number_of_days, :category_id, :user_id, :stars)
+  end
 end
