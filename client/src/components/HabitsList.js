a0172860684ecabcaf1c9b9021ac1051d1b55e03
emorgan05 <emorgan05@gmail.com>
2018-03-16 11:10:07 -0400

    Making HabitsList a container that can access the store for the habits array

--- client/src/components/HabitsList.js
+++ client/src/components/HabitsList.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import HabitListItem from './HabitListItem';
 
-const HabitsList = ({habits, onHabitSelect}) => {
-  if(!habits) {
-    return (<h3>Loading...</h3>);
-  }
-
-  const habitListItems = habits.map((habit, index) => {
-      return (
-        <HabitListItem
-          onHabitSelect={onHabitSelect}
-          key={index}
-          habit={habit}
-        />
-      )
+class HabitsList extends Component {
+  render() {
+    if(!this.props.habits) {
+      return (<h3>Loading...</h3>);
     }
-  );
 
-  return (
-    <div className="habits-list">
-      <ul>
-        {habitListItems}
-      </ul>
-    </div>
-  )
+    const habitListItems = this.props.habits.map((habit, index) => {
+        return (
+          <HabitListItem
+            onHabitSelect={this.props.onHabitSelect}
+            key={index}
+            habit={habit}
+          />
+        )
+      }
+    );
+
+    return (
+      <div className="habits-list">
+        <ul>
+          {habitListItems}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default HabitsList;
+function mapStateToProps(state) {
+  return {
+    habits: state.habits
+  }
+}
+
+export default connect(mapStateToProps)(HabitsList);
