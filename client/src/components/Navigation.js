41c43eaf42c0bf58d095a0a99b3c6c877c87373f
emorgan05 <emorgan05@gmail.com>
2018-03-16 09:59:15 -0400

    Connecting Navigation to redux store to reflect whether user is signed in

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,37 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Navbar,
   NavbarBrand,
   Nav,
   NavItem,
   NavLink } from 'reactstrap';
 
-const Navigation = ({user}) => {
-  if(user) {
-    return (
-      <Navbar color="faded" light expand="md">
-        <NavbarBrand href="/">HabitTracker</NavbarBrand>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="#">Log Out</NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    );
-  } else {
-    return (
-      <Navbar color="faded" light expand="md">
-        <NavbarBrand href="/">HabitTracker</NavbarBrand>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="#">Sign Up</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Log In</NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    );
+class Navigation extends Component {
+  render() {
+    if(this.props.logged_in) {
+      return (
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">HabitTracker</NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="#">Log Out</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      );
+    } else {
+      return (
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">HabitTracker</NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="#">Sign Up</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Log In</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      );
+    }
   }
 }
 
-export default Navigation;
+function mapStateToProps(state) {
+  return {
+    logged_in: state.session
+  }
+}
+
+export default connect(mapStateToProps)(Navigation);
