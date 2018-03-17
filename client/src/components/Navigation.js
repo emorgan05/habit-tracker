ec3af76393aba51570edb128e485cf1f8cb76e84
emorgan05 <emorgan05@gmail.com>
2018-03-17 15:52:36 -0400

    Adding routes to index.js and logout function to Navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,36 +1,42 @@
-import React from 'react';
-import { Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink } from 'reactstrap';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-const Navigation = (props) => {
-  if(props.logged_in) {
-    return (
-      <Navbar color="faded" light expand="md">
-        <NavbarBrand href="/">HabitTracker</NavbarBrand>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="#">Log Out</NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    );
-  } else {
-    return (
-      <Navbar color="faded" light expand="md">
-        <NavbarBrand href="/">HabitTracker</NavbarBrand>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="#">Sign Up</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Log In</NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    );
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  logout = () => {
+
+  }
+  
+  render () {
+    if(this.props.logged_in) {
+      return (
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">HabitTracker</NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/logout" onClick={this.logout} >Log Out</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      );
+    } else {
+      return (
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">HabitTracker</NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Sign Up</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">Log In</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      );
+    }
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
@@ -12,7 +13,15 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store}/>
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={LoginPage} />
+        <Route path="/habits" component={HabitsList} />
+        <Route path="/habits/:id" component={HabitDetails} />
+        <Route path="/habits/new" component={AddHabit} />
+        <Route path="/logout" component={} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
