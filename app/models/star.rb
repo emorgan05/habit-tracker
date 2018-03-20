8e263d7e8271f199547311b1be868a3fadfc61fa
emorgan05 <emorgan05@gmail.com>
2018-03-20 14:36:36 -0400

    Creating model methods to find the current streak and the longest streak

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,4 +1,39 @@
 class Star < ApplicationRecord
   belongs_to :habit
   delegate :user, :to => :habit, :allow_nil => true
+
+  def self.find_max_streak(habit_id)
+    stars = Star.where(habit_id: habit_id)
+    days = stars.pluck(:day)
+    streak = 0
+    max_streak = 0
+
+    days.each_with_index do |number, index|
+      if number + 1 === days[index + 1]
+        streak += 1
+      else
+        max_streak = streak if streak > max_streak
+        streak = 0
+      end
+    end
+
+    max_streak + 1
+  end
+
+  def self.find_current_streak(habit_id)
+    stars = Star.where(habit_id: habit_id)
+    days = stars.pluck(:day)
+    array = days.reverse
+    streak = 0
+
+    array.each_with_index do |n, index|
+      if n - 1 === array[index + 1]
+        streak += 1
+      else
+        break
+      end
+    end
+
+    streak + 1
+  end
 end
