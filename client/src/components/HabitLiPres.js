98e35593bfaed41688ee967cf9af19142d3f27c9
emorgan05 <emorgan05@gmail.com>
2018-03-22 10:18:19 -0400

    Removing import of icon because unused

--- client/src/components/HabitLiPres.js
+++ client/src/components/HabitLiPres.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
-import FaCircleO from 'react-icons/lib/fa/circle-o';
 
 const HabitLiPres = ({habit_id, name, handleClick}) => {
   return (
