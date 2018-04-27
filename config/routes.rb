ec3b388d70b07eb38fc6aed8d5481e818a814153
emorgan05 <emorgan05@gmail.com>
2018-04-27 14:59:00 -0400

    Changing config/routes. Working on making app work in deployment

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, :habits, :stars, :categories
+      mount Knock::Engine => "/knock"
       post 'user_token' => 'user_token#create'
     end
-  end
-
-  mount Knock::Engine => "/knock"
+  end  
 end
