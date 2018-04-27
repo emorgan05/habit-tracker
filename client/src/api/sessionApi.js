cf944d8c9055a893e8d0baf2ca57cc79273ccb47
emorgan05 <emorgan05@gmail.com>
2018-04-27 14:40:02 -0400

    namespacing user_token for log in

--- client/src/api/sessionApi.js
+++ client/src/api/sessionApi.js
@@ -1,6 +1,6 @@
 class SessionApi {
   static login(credentials) {
-    const request = new Request('http://localhost:3001/user_token', {
+    const request = new Request('api/v1/user_token', {
       method: 'POST',
       headers: new Headers({
         'Content-Type': 'application/json'
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, :habits, :stars, :categories
+      post 'user_token' => 'user_token#create'
     end
   end
 
   mount Knock::Engine => "/knock"
-  post 'user_token' => 'user_token#create'
 end
