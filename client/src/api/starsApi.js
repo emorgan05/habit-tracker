29286f6733ca0ca505b8613ff46b020550a1978f
emorgan05 <emorgan05@gmail.com>
2018-04-30 13:04:17 -0400

    Heroku is creating an error by posting new habits and stars to herokuapp.com/habits/api/v1/___ and I'm trying to debug

--- client/src/api/starsApi.js
+++ client/src/api/starsApi.js
@@ -5,7 +5,7 @@ class StarsApi {
 
   static createNewStar(star) {
     const headers = Object.assign({'Content-Type': 'application/json'}, this.requestHeaders());
-    const request = new Request('https://salty-cove-50283.herokuapp.com/api/v1/stars', {
+    const request = new Request('https://salty-cove-50283.herokuapp.com/habits/api/v1/stars', {
       method: 'POST',
       headers: headers,
       body: JSON.stringify({star: star})
