e0eb1a6f3e3b3e97477011ad7ed418fc0608a5ed
emorgan05 <emorgan05@gmail.com>
2018-05-02 11:09:54 -0400

    Updating cors.rb file to the herokuapp url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5000'
+    origins 'https://salty-cove-50283.herokuapp.com/'
 
     resource '*',
       headers: :any,
