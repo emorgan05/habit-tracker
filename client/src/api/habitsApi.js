46e05e0a155846101f7df08a271c1ff08eba4fa2
emorgan05 <emorgan05@gmail.com>
2018-05-02 11:48:39 -0400

    Seems to be an SSL issue, so I changed https to http

--- client/src/api/habitsApi.js
+++ client/src/api/habitsApi.js
@@ -17,7 +17,7 @@ class HabitsApi {
 
   static createHabit(habit) {
     const headers = Object.assign({'Content-Type': 'application/json'}, this.requestHeaders());
-    const request = new Request('https://www.salty-cove-50283.herokuapp.com/api/v1/habits', {
+    const request = new Request('http://www.salty-cove-50283.herokuapp.com/api/v1/habits', {
       method: 'POST',
       headers: headers,
       body: JSON.stringify({habit: habit})
